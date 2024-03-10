1ce0e064861e839b471b41944cf25768bce96f2e
rach <rachelpytse@gmail.com>
2024-03-10 20:14:18 +0100

    Added archive function

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,63 @@ import {v} from "convex/values"
 import {mutation, query} from "./_generated/server"
 import {Doc, Id} from "./_generated/dataModel"
 
+export const archive = mutation({
+    //pase the argument document id that we want to archive
+    args: {id: v.id("documents")},
+    handler: async (ctx, args) => {
+        //check identity id
+        const identity = await ctx.auth.getUserIdentity()
+
+        if(!identity) {
+            throw new Error("Not authenticated")
+        }
+
+        const userId = identity.subject
+
+        //fetch the parent document
+        const existingDocument = await ctx.db.get(args.id)
+
+        if(!existingDocument) {
+            throw new Error("Not found")
+        }
+
+        if(existingDocument.userId !== userId) {
+            throw new Error("Unauthorized")
+        }
+
+        const recursiveArchive = async(documentId: Id<"documents">) => {
+            const children = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) => (
+                    q
+                        .eq("userId", userId)
+                        .eq("parentDocument", documentId)
+                ))
+                .collect()
+
+                //use for because cannnot do async promise in map or forEach
+                for (const child of children) {
+                    await ctx.db.patch(child._id, {
+                        isAchived: true,
+                    })
+                    //check every child one more time that they don't have any children as well
+                    // so need to repeat the recursiveArchive function inside of itself to go to the end of the nested child
+                    await recursiveArchive(child._id)
+                }
+        }
+        // archive the document, then we modify the data instead of the object
+        const document = await ctx.db.patch(args.id, {
+            isAchived: true,
+        })
+
+        //after modify the main document, we pass the argument id inside of the recursiveArchive function which attempts to fetch all the children which have that id as their parentDocument
+        //and then we run a for loop over all those children and we change them to be archived as well
+        recursiveArchive(args.id)
+
+        return document
+    }
+})
+
 export const getSidebar = query({
     args: {
         parentDocument: v.optional(v.id("documents"))
