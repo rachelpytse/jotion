832067e35b39b470819f178bff7606060562b5b6
rach <rachelpytse@gmail.com>
2024-03-10 10:30:52 +0100

    Added landing page

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image"
+
+export const Heroes = () => {
+    return(
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                    <Image
+                        alt="Documents"
+                        src="/documents.png"
+                        fill
+                        className="object-contain dark:hidden"
+                    />
+                    <Image
+                        alt="Documents"
+                        src="/documents-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                    />
+                </div>
+                <div className="relative h-[400px] w-[400px] hidden md:block">
+                    <Image
+                        alt="Reading"
+                        src="/reading.png"
+                        fill
+                        className="object-contain dark:hidden"
+                    />
+                    <Image
+                        alt="Reading"
+                        src="/reading-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
