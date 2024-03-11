4c002bfb30ded1ffe779c58121604c238edb27ea
rach <rachelpytse@gmail.com>
2024-03-11 14:12:33 +0100

    Added cover image

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import { Doc } from "@/convex/_generated/dataModel"
 import { Button } from "./ui/button";
 import { api } from "@/convex/_generated/api";
 import { IconPicker } from "./icon-picker";
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
     initialData: Doc<"documents">;
@@ -24,6 +25,8 @@ export const Toolbar = ({
 
     const update = useMutation(api.documents.update)
     const removeIcon = useMutation(api.documents.removeIcon)
+
+    const coverImage = useCoverImage()
 
     const enableInput = () => {
         //a guest should not be able to edit
@@ -109,7 +112,7 @@ export const Toolbar = ({
                 )}
                 {!initialData.coverImage && !preview && (
                     <Button
-                        onClick={() => {}}
+                        onClick={coverImage.onOpen}
                         className="text-muted-foreground text-xs"
                         variant="outline"
                         size="sm"
