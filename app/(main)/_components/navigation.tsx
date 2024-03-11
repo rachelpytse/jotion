fb156c7a1b255c1a29dfe087c7b1b4abb12e3bdc
rach <rachelpytse@gmail.com>
2024-03-11 08:40:20 +0100

    Added search dialog

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,6 +13,7 @@ import {
     PopoverTrigger,
     PopoverContent
 } from "@/components/ui/popover"
+import { useSearch } from "@/hooks/use-search"
 // not using tailwind breakpoint because it's complex especially the sidebar is resizable on drag
 // use this to consider manually in javascript what is mobile and what is desktop
 import { UserItem } from "./user-item"
@@ -21,6 +22,7 @@ import { DocumentList } from "./document-list"
 import { TrashBox } from "./trash-box"
 
 export const Navigation = () => {
+    const search = useSearch()
     // in mobile mode when user click on a specific document it will collapse the sidebar because the sidebar takes much space
     const pathname = usePathname()
     const isMobile = useMediaQuery("(max-width: 768px)")
@@ -145,7 +147,7 @@ export const Navigation = () => {
                             label="Search"
                             icon={Search}
                             isSearch
-                            onClick={() => {}}
+                            onClick={search.onOpen}
                         />
                         <Item
                             label="Settings"
---
+++ components/search-command.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { File } from "lucide-react"
+import { useQuery } from "convex/react"
+import { useRouter } from "next/navigation"
+import { useUser } from "@clerk/clerk-react"
+
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList
+} from "@/components/ui/command"
+
+import { useSearch } from "@/hooks/use-search"
+import { api } from "@/convex/_generated/api"
+
+export const SearchCommand = () => {
+    const {user} = useUser()
+    const router = useRouter()
+    const documents = useQuery(api.documents.getSearch)
+    const [isMounted, setIsMounted] = useState(false)
+
+    const toggle = useSearch((store) => store.toggle)
+    const isOpen = useSearch((store) => store.isOpen)
+    const onClose = useSearch((store) => store.onClose)
+
+    //prevent hydration error in dialog ex. command uses dialog in the background
+    //because even it is marked "use client" nextjs is still going to do some server side on it first
+    //with dialog which can appear dynamically ex. search command can appear on a shortcut that can cause hydration error
+    //because in server side it still not exists but in client side all of sudden it exists and that cause hydration error
+    //we prevent this by not even allowing it to be rendered on the server side even though it's marked "use client" it's not enough
+    //client still does some server side rendering
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            //for mac and window
+            if(e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault()
+                toggle()
+            }
+        }
+
+        document.addEventListener("keydown", down)
+        return () => document.removeEventListener("keydown", down)
+    }, [toggle])
+
+    const onSelect = (id: string) => {
+        router.push(`/documents/${id}`)
+        onClose()
+    }
+
+    if(!isMounted) {
+        return null
+    }
+
+    return (
+        <CommandDialog open={isOpen} onOpenChange={onClose}>
+            <CommandInput
+                placeholder={`Search ${user?.fullName}'s Jotion...`}
+            />
+            <CommandList>
+                <CommandEmpty>Not results found.</CommandEmpty>
+                <CommandGroup heading="Documents">
+                    {documents?.map((document) => (
+                        <CommandItem
+                            key={document._id}
+                            value={`${document._id}-${document.title}`}
+                            title={document.title}
+                            onSelect={onSelect}
+                        >
+                            {document.icon ? (
+                                <p className="mr-2 text-[18px]">
+                                    {document.icon}
+                                </p>
+                            ) : (
+                                <File className="h-4 w-4 mr-2"/>
+                            )}
+                            <span>
+                                {document.title}
+                            </span>
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
+            </CommandList>
+        </CommandDialog>
+    )
+}
